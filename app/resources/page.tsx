9221096cf1d1c1b84cd47cc0ce5865541083dd9d
brittany-art <brittany@mccoyconsultingllc.com>
2026-06-11 11:10:27 -0500

    Update page.tsx

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -145,7 +145,6 @@ const partners = [
     items: [
       { name: "Good Hope Veterinary Hospital", href: "https://www.facebook.com/goodhopevet/" },
       { name: "Many Veterinary Hospital", href: "https://www.manyvet.com/" },
-      { name: "Red River Veterinary Center", href: ""https://www.google.com/search?q=Red+River+Veterinary+Center+Coushatta+LA"" },
     ],
     color: "#0099FF",
   },
